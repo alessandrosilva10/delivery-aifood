d54005206c97bddc369567a2ec3ce9bfa84c54cb
Alessandro <alessandro.mpgh@gmail.com>
2021-05-05 13:12:16 -0300

    meta tag with seo

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'google-site-verification', content: 'r8txx7XTw0F7y8RSKt58RbavSWB0oXLUQmuUDi2Bsng' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
