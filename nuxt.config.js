ed4cea640e8b528fd47d355be33fd2fd43674240
Alessandro <alessandro.mpgh@gmail.com>
2021-05-05 12:42:53 -0300

    Added seo

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,10 +37,12 @@ export default {
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-
+  pwa: {
+    icon: false // disables the icon module
+  },
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-
+    '@nuxtjs/pwa',
    ],
    transpileDependencies: ['vuex-persist'],
   // Modules: https://go.nuxtjs.dev/config-modules
