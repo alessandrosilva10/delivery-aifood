1e86e77f8b4781ab36b6cb2d989118a2d22a64e9
Alessandro <alessandro.mpgh@gmail.com>
2021-05-05 00:42:53 -0300

    a

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -168,6 +168,11 @@ export default {
       }
     }
 },
+beforeCreate(){
+  if(this.$auth.loggedIn){
+        this.$router.push("/");
+      }
+},
     methods: {
       async login(){
         if(this.verified){
